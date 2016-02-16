946d58841959145cf30d2c4f225529b238c77b27
aremis1984 <marinaleon1984@gmail.com>
2016-02-16 00:19:35 +0000

    Initial Commit

---
+++ primera_practica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  primera_practica
+//
+//  Created by Marina León on 16/2/16.
+//  Copyright © 2016 Marina León. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
